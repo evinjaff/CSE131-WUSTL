0d30393b083aa4ccffef57ca0adc3ebe5c076d20
Ron Cytron <roncytron@gmail.com>
2019-01-20 17:46:38 -0600

    Files for lab 4

---
+++ labs/lab4/Crest.java
@@ -0,0 +1,15 @@
+package lab4;
+
+import java.awt.Color;
+
+import sedgewick.StdDraw;
+
+public class Crest {
+	
+	public static void drawCrest() {
+		StdDraw.setPenRadius(0.08);
+		StdDraw.setPenColor(Color.GREEN);
+		StdDraw.rectangle(0.5, 0.5, 0.5, 0.5);
+	}
+
+}
