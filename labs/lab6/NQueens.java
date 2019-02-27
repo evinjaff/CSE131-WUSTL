07df3b29a541676167373e73b1a8afb46a272fd3
Ron Cytron <roncytron@gmail.com>
2019-02-27 09:55:05 -0600

    studio and lab 6

---
+++ labs/lab6/NQueens.java
@@ -0,0 +1,58 @@
+package lab6;
+
+public class NQueens {
+
+	
+	/**
+	 * Be sure to read the assignment first, before looking at this code
+	 * 
+	 *   
+	 *   Also, to call the code you wrote to detect if anything
+	 *   captures a given (row,col), you write:
+	 *   
+	 *       Capture.whoCaptures(board, row, col)
+	 *       
+	 *   and that returns either null if nothing captures (row,col)
+	 *   or it returns an int array of the row and column of a captor
+	 *   of (row,col)
+	 */
+	
+	 /**
+	 * Return a solution to the n queens problem in 
+	 * the form of a boolean array, where a true cell has a queen
+	 * and a false cell is empty
+	 * 
+	 * You would be wise to watch the videos and read the instructions
+	 *   about writing a helper method.
+	 *   
+	 * @param n, which is at least 4, so there is a solution
+	 * @return the solution
+	 */
+	public static boolean[][] findSoln(int n) {
+		QViz.setup(n);
+		boolean[][] ans = new boolean[n][n];
+		QViz.drawBoard(ans);
+		//
+		// FIXME
+		//
+		return ans;
+	}
+	
+	public static void nqueens(int n) {
+		boolean[][] board = findSoln(n);
+		if (board != null) {
+			System.out.println("Solution for " +n);
+			System.out.println(Utils.boardToString(board, -1, -1));
+			QViz.success(board);
+		}
+		else 
+			System.out.println("No solution for " + n);
+	}
+	
+	public static void main(String[] args0) {
+		for (int i=4; i < 11; ++i) {
+			nqueens(i);
+		}
+	}
+
+}
---
+++ studios/studio6/Circles.java
@@ -0,0 +1,16 @@
+package studio6;
+
+import cse131.NotYetImplementedException;
+import sedgewick.StdDraw;
+
+public class Circles {
+	private static void recursiveCircles(double xCenter, double yCenter, double radius) {
+		throw new NotYetImplementedException("delete this line of code and implement this method.");
+	}
+
+	public static void main(String[] args) {
+		StdDraw.setXscale(-1, +1);
+		StdDraw.setYscale(-1, +1);
+		recursiveCircles(0, 0, 0.5);
+	}
+}
