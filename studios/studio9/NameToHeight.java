db0a2e4aa1dfa119387865dca8593988dabca084
Ron Cytron <roncytron@gmail.com>
2019-04-04 11:02:36 -0500

    studio 9

---
+++ studios/studio9/NameToHeight.java
@@ -0,0 +1,25 @@
+package studio9;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import cse131.ArgsProcessor;
+import cse131.NotYetImplementedException;
+
+public class NameToHeight {
+	/**
+	 * Construct and fill a map with your studio group members' names, each
+	 * associated with his or her height.
+	 * 
+	 * Construct an ArgsProcessor and loop asking the args processor for a name to
+	 * lookup the height. When the user cancels (that is: when args processor
+	 * returns null), break from the loop. Otherwise, loop up the name in the map
+	 * and output the results. Be sure to handle the case where the map does not
+	 * contain a specified name.
+	 */
+	public static void main(String[] args) {
+		ArgsProcessor ap = new ArgsProcessor(args);
+		
+		throw new NotYetImplementedException("delete this line of code and implement this method.");
+	}
+}
