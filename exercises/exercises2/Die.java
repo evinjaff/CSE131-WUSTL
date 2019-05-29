c77d37f62df19df844d7f69ee31716da9aba0aeb
Evin Jaff <evinjaff@seattleacademy.org>
2019-05-29 09:46:01 -0700

    Completed Die exercise

--- exercises/exercises2/Die.java
+++ exercises/exercises2/Die.java
@@ -17,7 +17,34 @@ public class Die {
 		//  above integers as output, as if by 
 		//  throw of a die
 		//
-
+		// My solution was different than the professors, since he increments by less than, which is a little more efficient while I work with the idea of ranges. Regardless, I tested outputs and it seems to function like a normal dice
+		double output = Math.random();
+		if (output < 1.0/6.0) {
+			System.out.println("1");
+		}
+		if (output > 5.0/6.0) {
+			System.out.println("6");
+		}
+		if (output > 1.0/6.0) {
+			if (output < 2.0/6.0) {
+				System.out.println("2");
+			}
+		}
+		if (output > 2.0/6.0) {
+			if (output < 3.0/6.0) {
+				System.out.println("3");
+			}
+		}
+		if (output > 3.0/6.0) {
+			if (output < 4.0/6.0) {
+				System.out.println("4");
+			}
+		}
+		if (output > 4.0/6.0) {
+			if (output < 5.0/6.0) {
+				System.out.println("5");
+			}
+		}
 	}
 
 }
