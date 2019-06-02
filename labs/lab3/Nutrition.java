d28915867dd9c1422f59b7232d804a7210631c83
Evin Jaff <evinjaff@seattleacademy.org>
2019-06-02 13:19:07 -0700

    Created Lab 3 Class

--- labs/lab3/Nutrition.java
+++ labs/lab3/Nutrition.java
@@ -1,10 +1,13 @@
 package lab3;
 
+import cse131.ArgsProcessor;
+
 public class Nutrition {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		ArgsProcessor ap = new ArgsProcessor(args);
+		int money = ap.nextInt("Enter Starting Money");
 	}
 
 }
