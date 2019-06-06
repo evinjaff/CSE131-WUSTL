c826ee16cea26cdee87d53d61299701765d7a612
Evin Jaff <evinjaff@seattleacademy.org>
2019-06-05 19:46:53 -0700

    Completed Lab 1, and Exercise 1

--- exercises/exercises1/Change.java
+++ exercises/exercises1/Change.java
@@ -8,8 +8,72 @@ public class Change {
 		//
 		// Below, prompt the user to enter a number of pennies
 		//
+		ArgsProcessor ap = new ArgsProcessor(args);
+		int pennies = ap.nextInt("Enter a number of pennies");
+		int dollarBills = 0;
+		int quarters = 0;
+		int dimes = 0;
+		int nickels = 0;
+		int returnPennies = pennies;
 		
 		
+		
+		while (pennies/100 >= 1) {
+			pennies = pennies - 100;
+			dollarBills++;
+		}
+		while (pennies/25 > 0) {
+			pennies = pennies - 25;
+			quarters++;
+			
+		}
+		while (pennies/10 >= 1) {
+			pennies = pennies - 10;
+			dimes++;
+		}
+		while (pennies/5 >= 1) {
+			pennies = pennies - 5;
+			nickels++;
+		}
+		 
+		if(returnPennies == 1) {
+		System.out.println(returnPennies + " penny can be converted into");
+		}
+		else {
+			System.out.println(returnPennies + " pennies can be converted into");
+		}
+		
+		if(dollarBills == 1) {
+			System.out.println(dollarBills + " dollar");
+		}
+		else {
+			System.out.println(dollarBills + " dollars");
+		}
+		
+		if(quarters == 1) {
+		System.out.println(quarters + " quarter");
+		}
+		else {
+			System.out.println(quarters + " quarters");
+		}
+		if(dimes == 1) {
+		System.out.println(dimes + " dime");
+		}
+		else {
+			System.out.println(dimes + " dimes");
+		}
+		if(nickels == 1) {
+		System.out.println(nickels + " nickel");
+		}
+		else {
+			System.out.println(nickels + " nickels");
+		}
+		if(pennies == 1) {
+		System.out.println(pennies + " penny leftover");
+		}
+		else {
+			System.out.println(pennies + " pennies leftover");
+		}
 		//
 		// Then, compute and print out how many 
 		//    dollars, quarters, dimes, nickels, and pennies
--- labs/lab1/Nutrition.java
+++ labs/lab1/Nutrition.java
@@ -12,30 +12,43 @@ public class Nutrition {
 		double fat = ap.nextDouble("Enter amount of fat in grams");
 		double protein = ap.nextDouble("Enter amount of protein in grams");
 		int statedCals = ap.nextInt("Enter amount of calories");
+		double fiber = -(statedCals-((carbs*4)+(fat*9)+(protein*4)));
 		
-		double fatPercent = ((fat*9)/statedCals);
-		double fiber = (statedCals)-((carbs*4)+(fat*9)+(protein*4));
-		double proteinPercent = (((protein*4)/statedCals)*100);
-		double fiberPercent = ((fiber/statedCals)*100);
-		double carbPercent = ((carbs*4)/statedCals);
+		double proteinRound = (Math.round(protein*10))/10;
+		double fiberRound = (Math.round(fiber*10))/10;
+		double carbRound = (Math.round(carbs*10)/10);
+		double fatRound = (Math.round(fat*10)/10);
+		
+		double carbCalories = ((carbs*4)/statedCals)*100;
+		
+		double fatPercent = Math.round(((((fat*9)/statedCals)*1000)));
+		double proteinPercent = Math.round((protein*4)/statedCals*100);
+		double fiberPercent = Math.round((fiberRound)/statedCals*100);
+		double carbPercent = (Math.round(carbCalories*10));
+		double carbRounded = carbPercent/10;
+		double proteinRounded = proteinPercent/10;
+		double fatRounded = fatPercent/10;
+		double fiberRounded = fiberPercent/10;
 		
 		boolean lowCarb = carbPercent < 0.25;
 		boolean lowFat = fatPercent < 0.15;
 		boolean coinFlip = Math.random() > 0.5;
 		
 		
+		
 		System.out.println(name + " has:");
-		System.out.println(carbs + " grams of carbs = " + carbs*4 + " calories");
-		System.out.println(fat + " grams of fat = " + fat*9 + " calories");
-		System.out.println(protein + " grams of protein = " + protein*4 + " calories");
-		System.out.println("This food has " + (statedCals-fiber) + " available calories");
-		System.out.println("With " + fiber + " unavailable calories, this food has " + fiber/4 + " grams of fiber");
+		System.out.println("\t" + carbs + " grams of carbs = " + carbRound*4 + " calories");
+		System.out.println("\t" + fat + " grams of fat = " + fatRound*9 + " calories");
+		System.out.println("\t" + protein + " grams of protein = " + proteinRound*4 + " calories");
+		System.out.println("This food has " + (statedCals-fiberRound) + " available calories");
+		System.out.println("With " + fiberRound + " unavailable calories, this food has " + fiberRound/4 + " grams of fiber");
 		System.out.println("");
 		System.out.println("This means:");
-		System.out.println(carbPercent*100 + "% of the caloriess in the food are carbohydrates");
-		System.out.println(fatPercent*100 + "% of the calories in the food are fats");
-		System.out.println(proteinPercent + "% of the calories in the food is protein");
-		System.out.println(fiberPercent + "% of the calories in the food are fiber");
+		System.out.println("\t" + carbRounded + "% of the caloriess in the food are carbohydrates");
+		System.out.println("\t" + fatRounded+ "% of the calories in the food are fats");
+		System.out.println("\t" + proteinRounded + "% of the calories in the food is protein");
+		System.out.println("\t" + fiberRounded + "% of the calories in the food are fiber");
+		System.out.println("");
 		System.out.println("Is this food acceptable for a low-carb diet? " + lowCarb);
 		System.out.println("is this food acceptable for a low-fat diet? " + lowFat);
 		System.out.println("By a coin flip, should you eat this food? " + coinFlip);
