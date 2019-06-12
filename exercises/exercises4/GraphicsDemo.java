e25243621a6a96fb4df5f60a3500726665a67a9e
Evin Jaff <evinjaff@seattleacademy.org>
2019-06-12 00:14:05 -0700

    Completed GraphicsDemo and MouseFollower

--- exercises/exercises4/GraphicsDemo.java
+++ exercises/exercises4/GraphicsDemo.java
@@ -4,18 +4,41 @@ import java.awt.Color;
 
 import sedgewick.StdDraw;
 
+
 public class GraphicsDemo {
 
 	public static void main(String[] args) {
 		// blue point (look carefully, will be very small on your screen)
+		StdDraw.setPenColor(Color.BLUE);
+		StdDraw.point(.5, .25);
 		
 		// larger green point
+		StdDraw.setPenColor(Color.GREEN);
+		StdDraw.setPenRadius(.05);
+		StdDraw.point(.3, .6);
+		
 		
 		// unfilled red triangle
 		
+		StdDraw.setPenColor(Color.RED);
+		StdDraw.setPenRadius(.01);
+		
+		StdDraw.line(.1, .1, .2, .3);
+		StdDraw.line(.2, .3, .3, .1);
+		StdDraw.line(.3, .1, .1, .1);
+		
+		
 		// yellow circle, filled
+		StdDraw.setPenColor(Color.YELLOW);
+		
+		StdDraw.filledCircle(.8, .7, .1);
 
 		// filled blue rectangle
+		double rectx[] = {.4, .4, .7, .7};
+		double recty[] = {.4, .6, .6, .4};
+		
+		StdDraw.setPenColor(Color.BLUE);
+		StdDraw.filledPolygon(rectx, recty);
 		
 
 
--- exercises/exercises4/MouseFollower.java
+++ exercises/exercises4/MouseFollower.java
@@ -1,6 +1,7 @@
 package exercises4;
 
 import java.awt.Color;
+import sedgewick.StdDraw;
 
 import cse131.ArgsProcessor;
 import sedgewick.StdDraw;
@@ -8,13 +9,32 @@ import sedgewick.StdDraw;
 public class MouseFollower {
 
 	public static void main(String[] args) {
+		double x = StdDraw.mouseX();
+		double y = StdDraw.mouseY();
 		
+		double ballx = 0.5;
+		double bally = 0.5;
+		
+		StdDraw.filledCircle(ballx, bally, .1);
+		
+		boolean isPressed = StdDraw.mousePressed();
 		
 		while (true) {
+			isPressed = StdDraw.mousePressed();
+			
+			if(isPressed==true) {
 			StdDraw.clear();
+			x = StdDraw.mouseX();
+			y = StdDraw.mouseY();
+			
 			//
 			// Render one frame of your animation below here
 			//
+			
+			StdDraw.filledCircle(ballx, bally, .1);
+			
+			ballx = ballx + (x-ballx);
+			bally = bally + (y-bally);
 
 			
 			//  
@@ -23,8 +43,8 @@ public class MouseFollower {
 			// Stdraw.show(..) achieves double buffering and
 			//   avoids the tight spinning loop
 			StdDraw.show(10);
+			}
 		}
-
 	}
 
 }
