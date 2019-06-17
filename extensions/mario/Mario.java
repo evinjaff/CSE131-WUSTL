9e2e1129e632b761d092f544844e01b349a9bcfd
Evin Jaff <evinjaff@seattleacademy.org>
2019-06-17 13:54:54 -0700

    Made progress on Mario extension and Speeding extension

--- extensions/mario/Mario.java
+++ extensions/mario/Mario.java
@@ -5,7 +5,7 @@ import cse131.ArgsProcessor;
 public class Mario {
 
 	public static void main(String[] args) {
-		
+
 		//
 		// Surprise!  This part is done for you.
 		//    Don't change this and don't ask for any other inputs
@@ -14,15 +14,65 @@ public class Mario {
 		ArgsProcessor ap = new ArgsProcessor(args);
 		int size    = ap.nextInt("What size mountain do you want?");
 		int pattern = ap.nextInt("What pattern (1, 2, 3, or 4)?");
-		
+
 		if (size < 1)
 			throw new IllegalArgumentException("Size must be at least 1");
 		if (pattern < 1 || pattern > 4)
 			throw new IllegalArgumentException("Invalid pattern, must be 1, 2, 3, or 4.  Mario aborts");
-		
+
 		//
 		// Create the mountain by printing to System.out
 		//
+		if(pattern == 1) {
+			for(int row=0;row<size;row++) {
+				for(int col=size-1;col>row;col--) {
+					System.out.print(" ");
+				}
+				for(int platformCt=-1;platformCt<row;platformCt++) {
+					System.out.print("#");
+				}
+				//System.out.print("#");
+				System.out.println("");
+			}
+
+		}
+		if(pattern == 2) {
+			for(int i=0;i<size+1;i++) {
+				for(int k=0;k<i;k++) {
+					System.out.print("#");
+				}
+				System.out.println("");
+			}
+
+		}
+		if(pattern == 3) {
+			for(int i=0;i<size;i++) {
+				for(int j=0; j<i;j++) {
+					//creates needed spaces
+					System.out.print(" ");
+				}
+				
+				
+				for(int k=size; k>i ;k--) {
+					//runs inverse to the i for loop to create the #'s
+					System.out.print("#");
+				}
+				System.out.println("");
+			}
+
+		}
+		
+		if(pattern == 4) {
+			for(int i=0;i<=size;i++) { 
+				
+				for(int k=size;k>i;k--) {
+					System.out.print("#");
+					//Does all the spaces
+				}
+				System.out.println("");
+			}
+
+		}
 
 	}
 
--- extensions/speeding/Speeding.java
+++ extensions/speeding/Speeding.java
@@ -10,9 +10,14 @@ public class Speeding {
 		int carSpeed = ap.nextInt("How fast were you going?");
 		int speedLimit = ap.nextInt("What was the speed limit?");
 		
+		if (carSpeed<speedLimit) {
 		int finableAmount = ((carSpeed-(speedLimit+10))*10)+50;
 		
 		System.out.println("You owe $" + finableAmount);
+		}
+		else {
+			System.out.println("");
+		}
 	}
 
 }
