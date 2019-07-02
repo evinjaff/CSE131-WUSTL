9a209d54c0c125b95209c8c8715ec15bf8c27fbd
Evin Jaff <evinjaff@seattleacademy.org>
2019-07-01 20:50:50 -0700

    Completed Studio 5

--- studios/studio5/Methods.java
+++ studios/studio5/Methods.java
@@ -19,8 +19,31 @@ public class Methods {
 	 * @return the product of the factors
 	 */
 	public static int mpy(int x, int y) {
-		return 0;  // FIXME
+		return x*y;  // FIXME
+	}
+	
+	public static double avg3(int x, int y, int z) {
+		return (x+y+z)/3.0;
+	}
+	
+	public static double sumArray(double[] a) {
+		double sum = 0.0;
+		for(int i=0;i<a.length;i++) {
+			sum += a[i];
+		}
+		return sum;
 	}
 
-
+	public static double average(double [] a) {
+		double sum = sumArray(a);
+		return sum/(double)(a.length);
+	}
+	public static String pig(String n) {
+		//n.substring(1);
+		n = n.substring(1) + n.substring(0, 1) + "ay";
+		return n;
+	}
+	public static double division(int x, int y) {
+		return x/y;
+	}
 }
